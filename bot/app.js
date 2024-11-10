9b3802d0199b00da62a1bd184060ef754840b839
h8develop <kupcov2005@icloud.com>
2024-11-10 18:41:01 +0300

    ааааа

--- bot/app.js
+++ bot/app.js
@@ -1,7 +1,7 @@
 import { Telegraf, Markup } from 'telegraf'
 
-const token = '7875155444:AAHtt-znGGNvWhkTBA8SFgE6InDRaDMkYPk'
-const webAppUrl = 'https://lizard-clicker-pro.web.app'
+const token = '8124471087:AAGp_FGoAR3D-jMtzpGKlEG_DDNGz4Y69vo'
+const webAppUrl = 'https://pbprojeck-87266.web.app'
 
 const bot = new Telegraf(token)
 
