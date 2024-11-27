fff26be8f591f9e8fa9f55afe0b3e3134a65dcb7
h8develop <kupcov2005@icloud.com>
2024-11-27 23:34:17 +0300

    bot

--- bot/app.js
+++ bot/app.js
@@ -1,15 +1,8 @@
 // bot/app.js
 
 import { Telegraf, Markup } from 'telegraf';
-import dotenv from 'dotenv';
-import logger from '/logger.js';
-
-dotenv.config();
 
 const token = "8124471087:AAGp_FGoAR3D-jMtzpGKlEG_DDNGz4Y69vo"
-if (!token) {
-  throw new Error('BOT_TOKEN не определён в переменных окружения.');
-}
 const webAppUrl = 'https://pbprojeck-87266.web.app';
 
 const bot = new Telegraf(token);
